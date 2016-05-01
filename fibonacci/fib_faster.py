01af291cc6262701b6541fd3e55a624cf2a80bc8
bart.lee <bart.lee@daumkakao.com>
2016-05-01 23:34:27 +0900

    p1, p2 (fi)

--- fibonacci/fib_faster.py
+++ fibonacci/fib_faster.py
@@ -1,19 +1,5 @@
 # Uses python3
-from time import sleep, perf_counter as pc
-
-t0 = pc()
-
-
-
-def calc_fib(n):
-    if (n <= 1):
-        return n
-
-    return calc_fib(n - 1) + calc_fib(n - 2)
-
-
 dic = {'key': 0}
-
 
 def calc_fib_more_faster(n):
     if (n <= 1):
@@ -38,7 +24,4 @@ n = int(input())
 # print(calc_fib(n))
 # print(pc()-t0)
 
-t0 = pc()
 print(calc_fib_more_faster(n))
-print(pc()-t0)
-
--- fibonacci_last_digit/fibonacci_last_digit.py
+++ fibonacci_last_digit/fibonacci_last_digit.py
@@ -2,8 +2,26 @@
 import sys
 
 def get_fibonacci_last_digit(n):
-    # write your code here
-    return 0
+    if (n <= 1):
+        return n
+
+    prev_num = 1
+    prev_prev_num = 0
+
+    for i in range(2, n):
+        temp_pp_num = prev_prev_num
+        prev_prev_num = prev_num
+        prev_num = prev_num + temp_pp_num
+        if prev_num > 9:
+            prev_num = prev_num % 10
+
+        if prev_prev_num > 9:
+            prev_prev_num = prev_prev_num % 10
+
+    if (prev_num + prev_prev_num) > 9:
+        return (prev_num + prev_prev_num) % 10
+    else:
+        return prev_num + prev_prev_num
 
 if __name__ == '__main__':
     input = sys.stdin.read()
