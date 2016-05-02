1f1eb9c30a86db4f8e968d28acda02246525f8f3
bart.lee <bart.lee@daumkakao.com>
2016-05-02 19:18:20 +0900

    lcm

--- gcd/gcd.py
+++ gcd/gcd.py
@@ -1,16 +1,36 @@
 # Uses python3
 import sys
 
-def gcd(a, b):
-    current_gcd = 1
-    for d in range(2, min(a, b) + 1):
-        if a % d == 0 and b % d == 0:
-            if d > current_gcd:
-                current_gcd = d
+# def gcd(a, b):
+#     current_gcd = 1
+#     for d in range(2, min(a, b) + 1):
+#         if a % d == 0 and b % d == 0:
+#             if d > current_gcd:
+#                 current_gcd = d
+#
+#     return current_gcd
 
-    return current_gcd
+def u_gcd(a, b):
+
+    if (a == 1 or b == 1):
+        return 1
+
+    for d in range(2, min(a, b) + 1):
+        if (a > b):
+            s = a % b
+            if s == 0:
+                return b
+            else:
+                a = s
+        else:
+            s = b % a
+            if s == 0:
+                return a
+            else:
+                b = s
 
 if __name__ == "__main__":
     input = sys.stdin.read()
     a, b = map(int, input.split())
-    print(gcd(a, b))
+    # print(gcd(a, b))
+    print(u_gcd(a, b))
--- lcm/lcm.py
+++ lcm/lcm.py
@@ -1,9 +1,33 @@
 # Uses python3
 import sys
 
+def u_gcd(a, b):
+
+    if (a == 1 or b == 1):
+        return 1
+
+    for d in range(2, min(a, b) + 1):
+        if (a > b):
+            s = a % b
+            if s == 0:
+                return b
+            else:
+                a = s
+        else:
+            s = b % a
+            if s == 0:
+                return a
+            else:
+                b = s
+
 def lcm(a, b):
-    #write your code here
-    return a*b
+    gcd = u_gcd(a, b)
+    if (a > b):
+        a = int(a / gcd)
+    else:
+        b = int(b / gcd)
+
+    return a * b
 
 if __name__ == '__main__':
     input = sys.stdin.read()
